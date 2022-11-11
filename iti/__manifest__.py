3a5f981783d6c4ef5d94ff8cc35de8248fe30a3e
Noha Shehab <nohashehab.iti@gmail.com>
2022-11-12 01:37:17 +0200

    odoo security rules

--- iti/__manifest__.py
+++ iti/__manifest__.py
@@ -3,6 +3,8 @@
     "depends":[ "hr"],
     ## for views
     "data":[
+        'secuirty/iti_secuirty.xml',
+        'secuirty/ir.model.access.csv',
         'views/iti_students_views_day02.xml',
         'views/iti_tracks_views.xml',
         'views/hr_employees_views.xml'

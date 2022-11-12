4f67f553f9cceabed777b1f4765e30bb1ec0ccf8
Noha Shehab <nohashehab.iti@gmail.com>
2022-11-12 23:16:31 +0200

    print report

--- iti/__manifest__.py
+++ iti/__manifest__.py
@@ -7,6 +7,8 @@
         'secuirty/ir.model.access.csv',
         'views/iti_students_views_day02.xml',
         'views/iti_tracks_views.xml',
-        'views/hr_employees_views.xml'
+        'views/hr_employees_views.xml',
+        'reports/iti_templates.xml',
+        'reports/iti_reports.xml'
     ]
 }
